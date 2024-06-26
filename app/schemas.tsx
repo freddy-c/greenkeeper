78f4b59be6bab3ec34aa61be89541da66d6b7bac
Freddy Cunningham <freddy@fcunningham.co.uk>
2024-06-26 10:56:36 +0100

    Add form for adding an application

--- app/schemas.tsx
+++ app/schemas.tsx
@@ -26,3 +26,39 @@ export const AddProductSchema = z.object({
         label: z.string(),
     }),
 });
+
+const ApplicationItemSchema = z.object({
+    itemId: z.string().uuid(),
+    quantity: z.coerce.number().min(0, "Quantity must be non-negative"),
+});
+
+export const AddApplicationSchema = z.object({
+    date: z.string().date(),
+    operator: z.string().min(1, "Operator name is required"),
+    waterVolume: z.number().min(0, "Water volume must be non-negative"),
+    temperature: z
+        .number()
+        .min(-50, "Temperature seems too low")
+        .max(60, "Temperature seems too high"),
+    windSpeed: z.number().min(0, "Wind speed must be non-negative"),
+    observations: z
+        .string()
+        .max(160, {
+            message: "Observations must not be longer than 160 characters.",
+        })
+        .optional(),
+    items: z
+        .array(ApplicationItemSchema)
+        .min(1, "At least one item is required"),
+});
+
+export const SprayCalculatorSchema = z.object({
+    referencePressure: z.number().min(0, "Pressure must be non-negative"),
+    referenceNozzleCapacity: z
+        .number()
+        .min(0, "Nozzle capacity must be non-negative"),
+    forwardSpeed: z.number().min(0, "Forward speed must be non-negative"),
+    waterVolume: z.number().min(0, "Water volume must be non-negative"),
+    applicationRate: z.number().min(0, "Application rate must be non-negative"),
+    nozzleSpacing: z.number().min(0, "Nozzle spacing must be non-negative"),
+});
